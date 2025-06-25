f0da07b304d4357a69f363e6a69e282ef0ff20cb
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2025-06-25 20:57:48 +0530

    create new table

--- gopal/admin.py
+++ gopal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Empstaff
+from .models import Empstaff, OrdStk
 
 class EmpstaffAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'admin_image_preview','mobileno', 'wunit', 'workunit', 'hostel',)
@@ -11,6 +11,18 @@ class EmpstaffAdmin(admin.ModelAdmin):
         return super().get_queryset(request).using('mssql')
         return super().get_queryset(request).using('demo')
 
+class ordStkAdmin(admin.ModelAdmin):
+    list_display = ('trstype', 'total', 'unit', 'jobno', 'tb', 'clr', 'bc', 'sew', 'che', 'irn', 'pack', 'oth', 'mist',
+                    'trstype_all', 'deldt', 'merch', 'admin_image_preview', 'tbimage',
+                    'director_sample_order', 'finaldelvdate', 'final_year_delivery', 'insdatenew', 'c')
+    search_fields = ('jobno',)
+    list_filter = ('trstype',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).using('mssql')
+        return super().get_queryset(request).using('demo')
+
 admin.site.register(Empstaff, EmpstaffAdmin)
+admin.site.register(OrdStk, ordStkAdmin)
 
 # Register your models here.
