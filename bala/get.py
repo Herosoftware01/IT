e10394eae235ef17ec8f131ce351b8e23bb4d0f5
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2025-06-26 19:42:02 +0530

    Ord_Sample Status added

--- bala/get.py
+++ bala/get.py
@@ -18,23 +18,53 @@ from django.db import models
 
 
 
-class EmpAttendanceFact(models.Model):
+class OrdSampleStatus(models.Model):
 
-    code_emb_attendance_fact = models.IntegerField(db_column='Code Emb Attendance Fact', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    print = models.CharField(db_column='Print', max_length=750)  # Field name made lowercase.
 
-    date = models.DateTimeField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
+    emb = models.CharField(db_column='Emb', max_length=750)  # Field name made lowercase.
 
-    name = models.CharField(max_length=100, blank=True, null=True)
+    date = models.DateTimeField(db_column='DATE', blank=True, null=True)  # Field name made lowercase.
 
-    intime = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(db_column='Status', max_length=20, blank=True, null=True)  # Field name made lowercase.
 
-    outtime = models.DateTimeField(blank=True, null=True)
+    image = models.CharField(db_column='Image', max_length=8000, blank=True, null=True)  # Field name made lowercase.
 
-    emppic = models.CharField(db_column='Emppic', max_length=8000, blank=True, null=True)  # Field name made lowercase.
+    topbottomimg = models.CharField(db_column='TopBottomImg', max_length=8000, blank=True, null=True)  # Field name made lowercase.
 
-    con_code_name_in_out = models.CharField(db_column='Con_Code_name_in_out', max_length=4000, blank=True, null=True)  # Field name made lowercase.
+    remarks = models.CharField(db_column='Remarks', max_length=150)  # Field name made lowercase.
 
-    rel_code_name = models.CharField(db_column='Rel_code_name', max_length=112, blank=True, null=True)  # Field name made lowercase.
+    stock = models.IntegerField(db_column='Stock')  # Field name made lowercase.
+
+    cutqty = models.IntegerField(db_column='CutQty')  # Field name made lowercase.
+
+    active = models.CharField(db_column='Active', max_length=10, blank=True, null=True)  # Field name made lowercase.
+
+    o_finaldelvdate = models.DateField(db_column='o_FinalDelvdate', blank=True, null=True)  # Field name made lowercase.
+
+    jobno = models.CharField(db_column='Jobno', max_length=50)  # Field name made lowercase.
+
+    merch = models.CharField(db_column='Merch', max_length=35, blank=True, null=True)  # Field name made lowercase.
+
+    buy = models.CharField(db_column='Buy', max_length=5, blank=True, null=True)  # Field name made lowercase.
+
+    buyer = models.CharField(db_column='Buyer', max_length=40, blank=True, null=True)  # Field name made lowercase.
+
+    sample_status = models.CharField(db_column='Sample Status', max_length=15, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    unitname = models.CharField(db_column='Unitname', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    topbottom_des = models.CharField(db_column='TopBottom_des', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    colour = models.CharField(db_column='Colour', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    sampletype = models.CharField(db_column='Sampletype', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    send_dt = models.DateTimeField(db_column='SEND_DT', blank=True, null=True)  # Field name made lowercase.
+
+    apr_dt = models.DateTimeField(db_column='APR_DT', blank=True, null=True)  # Field name made lowercase.
+
+    rej_dt = models.DateTimeField(db_column='REJ_DT', blank=True, null=True)  # Field name made lowercase.
 
 
 
@@ -42,5 +72,5 @@ class EmpAttendanceFact(models.Model):
 
         managed = False
 
-        db_table = 'Emp_Attendance_Fact'
+        db_table = 'Ord_Sample_status'
 
--- bala/models.py
+++ bala/models.py
@@ -82,4 +82,70 @@ class EmpAttendanceFact(models.Model):
 
     
 
-   
+class OrdSampleStatus(models.Model):
+
+    print = models.CharField(db_column='Print', max_length=750)  # Field name made lowercase.
+
+    emb = models.CharField(db_column='Emb', max_length=750)  # Field name made lowercase.
+
+    date = models.DateTimeField(db_column='DATE', blank=True, null=True)  # Field name made lowercase.
+
+    status = models.CharField(db_column='Status', max_length=20, blank=True, null=True)  # Field name made lowercase.
+
+    image = models.CharField(db_column='Image', max_length=8000, blank=True, null=True)  # Field name made lowercase.
+
+    topbottomimg = models.CharField(db_column='TopBottomImg', max_length=8000, blank=True, null=True)  # Field name made lowercase.
+
+    remarks = models.CharField(db_column='Remarks', max_length=150)  # Field name made lowercase.
+
+    stock = models.IntegerField(db_column='Stock')  # Field name made lowercase.
+
+    cutqty = models.IntegerField(db_column='CutQty')  # Field name made lowercase.
+
+    active = models.CharField(db_column='Active', max_length=10, blank=True, null=True)  # Field name made lowercase.
+
+    o_finaldelvdate = models.DateField(db_column='o_FinalDelvdate', blank=True, null=True)  # Field name made lowercase.
+
+    jobno = models.CharField(db_column='Jobno', max_length=50,primary_key=True)  # Field name made lowercase.
+
+    merch = models.CharField(db_column='Merch', max_length=35, blank=True, null=True)  # Field name made lowercase.
+
+    buy = models.CharField(db_column='Buy', max_length=5, blank=True, null=True)  # Field name made lowercase.
+
+    buyer = models.CharField(db_column='Buyer', max_length=40, blank=True, null=True)  # Field name made lowercase.
+
+    sample_status = models.CharField(db_column='Sample Status', max_length=15, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    unitname = models.CharField(db_column='Unitname', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    topbottom_des = models.CharField(db_column='TopBottom_des', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    colour = models.CharField(db_column='Colour', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    sampletype = models.CharField(db_column='Sampletype', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    send_dt = models.DateTimeField(db_column='SEND_DT', blank=True, null=True)  # Field name made lowercase.
+
+    apr_dt = models.DateTimeField(db_column='APR_DT', blank=True, null=True)  # Field name made lowercase.
+
+    rej_dt = models.DateTimeField(db_column='REJ_DT', blank=True, null=True)  # Field name made lowercase.
+
+
+
+    class Meta:
+
+        managed = False
+
+        db_table = 'Ord_Sample_status'
+    
+    def admin_image_preview(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image}" alt="" width="100" style="border: 1px solid #100; border-radius:10%;"/>')
+        return "No Image"
+    admin_image_preview.short_description = "Image"
+
+    def admin_image_preview1(self):
+        if self.topbottomimg:
+            return mark_safe(f'<img src="{self.topbottomimg}" alt="" width="100" style="border: 1px solid #100; border-radius:10%;"/>')
+        return "No Image"
+    admin_image_preview1.short_description = "TbImage"
