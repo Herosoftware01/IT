ee08f7078f34d1cf51f629e9aef7b6add9fbf719
Herosoftware01 <herosoftware@herofashion.com>
2025-06-29 12:31:59 +0530

    Vishwa Code Updated

---
+++ vishwa/database_router.py
@@ -0,0 +1,28 @@
+class MasterRouter:
+    models = {'*'}
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'vishwa':
+            if model._meta.model_name in self.models:
+                return 'demo'
+            return 'mssql'
+        return 'default'
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'vishwa':
+            if model._meta.model_name in self.models:
+                return 'demo'
+            return 'mssql'
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'vishwa':
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == 'vishwa':
+            if model_name in self.models:
+                return db == 'demo'
+            return db == 'mssql'
+        return db == 'default'
