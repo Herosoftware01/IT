3b9f112c9a6cc75e81dfc74750d867538b64b21c
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2025-06-29 12:13:51 +0530

    new update t_buyer

--- dhana/views.py
+++ dhana/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import VueYarnStock,PrintRgbAlt
-import pandas as pd
-import pygwalker as pyg
-from pygwalker import to_html
+from .models import VueYarnStock #PrintRgbAlt
+# import pandas as pd
+# import pygwalker as pyg
+# from pygwalker import to_html
 
-from pygwalker import walk
+# from pygwalker import walk
 
 
 # Create your views here.
@@ -33,26 +33,26 @@ def home(request):
 #     return render(request, 'dashboard.html', {'pyg_html': pyg_html})
 
 
-def dashboard(request):
-    queryset = PrintRgbAlt.objects.using('mssql').all()
-    data = list(queryset.values())
-    df = pd.DataFrame(data)
+# def dashboard(request):
+#     queryset = PrintRgbAlt.objects.using('mssql').all()
+#     data = list(queryset.values())
+#     df = pd.DataFrame(data)
 
-    # Optional: show image preview
-    if 'img_print' in df.columns:
-        df['img_print'] = df['img_print'].apply(
-            lambda url: f'<img src="{url}" width="80" height="80">' if pd.notna(url) else ''
-        )
+#     # Optional: show image preview
+#     if 'img_print' in df.columns:
+#         df['img_print'] = df['img_print'].apply(
+#             lambda url: f'<img src="{url}" width="80" height="80">' if pd.notna(url) else ''
+#         )
 
-    # Example pivot (replace columns accordingly)
-    if 'job_number' in df.columns and 'img_print' in df.columns:
-        pivot_df = pd.pivot_table(df, index='job_number', values='img_print', aggfunc=lambda x: ', '.join(x))
+#     # Example pivot (replace columns accordingly)
+#     if 'job_number' in df.columns and 'img_print' in df.columns:
+#         pivot_df = pd.pivot_table(df, index='job_number', values='img_print', aggfunc=lambda x: ', '.join(x))
 
-        table_html = pivot_df.to_html(escape=False)
-    else:
-        table_html = df.to_html(escape=False)
+#         table_html = pivot_df.to_html(escape=False)
+#     else:
+#         table_html = df.to_html(escape=False)
 
-    return render(request, 'dashboard.html', {'table_html': table_html})
+#     return render(request, 'dashboard.html', {'table_html': table_html})
 
 
 
